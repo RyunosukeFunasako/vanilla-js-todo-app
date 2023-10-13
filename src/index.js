9d36d56394a1284ed6a1471f52231e202d3a55cd
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2023-10-13 15:01:24 +0900

    add: index.jsの追加
    TODOタスク追加機能(テキスト表示)

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import "./styles.css" assert { type: 'css' };
+
+const onClickAdd = () => {
+    // テキストボックスの値を取得し初期化する
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+    
+    // divタグ生成
+    const div = document.createElement("div");
+    div.className = "todo";
+
+    // liタグ生成
+    const li = document.createElement("li");
+    li.innerText = inputText;
+
+    // button(完了)タグ生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+
+    // button(削除)タグ生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+
+    // divタグの子要素に各要素を設定
+    div.appendChild(li);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+
+    // 未完了リストに追加
+    document.getElementById("uncomplete-list").append(div);
+};
+
+document.getElementById("add-button").addEventListener("click", () => onClickAdd());
